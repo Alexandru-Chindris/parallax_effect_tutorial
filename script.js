ed732764d4788aa82c3a8a47435e08b7aff28337
Alexandru Chindris <alexandru.chindris.buisness@gmail.com>
2022-08-26 03:46:33 +0200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-let element = document.getElementById("moving");
-
-window.addEventListener("scroll", function(){
-    let ofset = window.pageYOffset;
-    element.style.backgroundPositionY = ofset *0.7 + "px";
-})
